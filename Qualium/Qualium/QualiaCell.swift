3e5b8b3e82602a6e567f862d97567fc30297161c
Atsushi Miyake <atsushi_main@outlook.jp>
2016-06-14 00:16:29 +0900

    implemented QualiumDelegate and DataSource

--- Qualium/Qualium/QualiaCell.swift
+++ Qualium/Qualium/QualiaCell.swift
@@ -9,6 +9,6 @@
 import Foundation
 import UIKit
 
-class QualiaCell: UIView {
+class QualiaCell: UICollectionViewCell {
     
 }
--- Qualium/Qualium/QualiumView.swift
+++ Qualium/Qualium/QualiumView.swift
@@ -19,6 +19,74 @@ protocol QualiumViewDataSource {
     func qualiumView(qualiumView: QualiumView, cellForQualiaAtIndexPath indexPath: NSIndexPath) -> QualiaCell
 }
 
+private let kMinimumLineSpacing: CGFloat        = 15.0
+private let kMinimumInterQualiaSpacing: CGFloat = 15.0
 class QualiumView: UIView {
-   
+    
+    private var collectionView: UICollectionView!
+    private var layout  = UICollectionViewFlowLayout()
+    private var qualias = [Qualia]()
+    var delegate: QualiumViewDelegate!     = nil
+    var dataSource: QualiumViewDataSource! = nil
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setup()
+    }
+    
+    private func setup() {
+        self.collectionViewFlowLayoutSetup()
+        self.collectionViewSetup()
+    }
+    
+    private func collectionViewSetup() {
+        self.collectionView = UICollectionView(frame: self.frame, collectionViewLayout: self.layout)
+        self.addSubview(self.collectionView)
+    }
+    
+    private func collectionViewFlowLayoutSetup() {
+        self.layout.minimumLineSpacing      = kMinimumLineSpacing
+        self.layout.minimumInteritemSpacing = kMinimumInterQualiaSpacing
+    }
+    
+    func reloadData(completionHandler: (() -> Void)?) {
+        self.collectionView.reloadData(complateHandler: completionHandler)
+    }
+
+}
+
+extension QualiumView: UICollectionViewDelegate {
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        self.delegate.qualiumView(self, didSelectQualiaAtIndexPath: indexPath)
+    }
+    
+}
+
+extension QualiumView: UICollectionViewDataSource {
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return self.dataSource.numberOfSectionInQualiumView(self)
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.dataSource.qualiumView(self, numberOfQualiasInSection: section)
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return self.dataSource.qualiumView(self, cellForQualiaAtIndexPath: indexPath)
+    }
+    
+}
+
+private extension UICollectionView {
+    func reloadData(complateHandler completionHandler: (() -> Void)?) {
+        self.reloadData()
+        completionHandler?()
+    }
 }
