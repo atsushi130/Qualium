0f3759d83b2ad9e206a665c54520e0e1d3c2ff97
= <=>
2016-06-13 21:30:41 +0900

    Initial Commit

---
+++ Qualium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Qualium
+//
+//  Created by ATSUSHI on 2016/06/13.
+//  Copyright © 2016年 ATSUSHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
