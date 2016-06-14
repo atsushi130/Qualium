2e15f18d2584c4f5b04b558c817a884af2d4fbc7
atsushi130 <atsushi_main@outlook.jp>
2016-06-14 22:36:13 +0900

    implemented sizeFit function

--- Qualium/Qualium/QualiaCell.swift
+++ Qualium/Qualium/QualiaCell.swift
@@ -15,8 +15,8 @@ struct Margin {
     static let Top: CGFloat    = 5.0
     static let Bottom: CGFloat = 5.0
     
-    static let Width  = Margin.Left + Margin.Right
-    static let HeightMargin = Margin.Top + Margin.Bottom
+    static let Width: CGFloat  = Margin.Left + Margin.Right
+    static let Height: CGFloat = Margin.Top + Margin.Bottom
 }
 
 private let kIconDiameter: CGFloat = 20.0
@@ -24,11 +24,10 @@ private let kCornerRadius: CGFloat = 10.0
 
 class QualiaCell: UICollectionViewCell {
     
-    @IBOutlet private var textView: UITextView!
-    @IBOutlet private var view: UIView!
-    let icon = UIImageView()
-    
-    var qualia = Qualia()
+    private let textView = UITextView()
+    private let view     = UIView()
+    let icon             = UIImageView()
+    var qualia           = Qualia()
     
     var text: String {
         get {
@@ -42,13 +41,33 @@ class QualiaCell: UICollectionViewCell {
     }
     
     var cellSize: CGSize {
-        return self.frame.size
+        return CGSizeMake(self.frame.width, self.view.frame.height)
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.setup()
+    }
+    
+    private func setup() {
+        self.viewSetup()
+        self.textViewSetup()
+        self.iconSetup()
+    }
+    
+    private func viewSetup() {
+        self.addSubview(self.view)
+    }
+    
+    private func textViewSetup() {
+        self.textView.frame = CGRectMake(Margin.Left, Margin.Top, 0, 0)
+        self.view.addSubview(self.textView)
+    }
+    
+    private func iconSetup() {
         self.icon.layer.cornerRadius  = kCornerRadius
         self.icon.layer.masksToBounds = true
+        self.addSubview(self.icon)
     }
 
     private func sizeFit() {
@@ -63,7 +82,8 @@ class QualiaCell: UICollectionViewCell {
         let dummyTextView  = UITextView()
         dummyTextView.font = self.textView.font
         dummyTextView.text = self.textView.text
-        let maxSize = CGSizeMake(200, 10000000)
+        let maxSize        = CGSizeMake(200, 10000000)
+        
         // textView in maxSize
         let size = (dummyTextView.text as NSString).boundingRectWithSize(maxSize,
                                                                          options: NSStringDrawingOptions.UsesLineFragmentOrigin,
@@ -74,16 +94,18 @@ class QualiaCell: UICollectionViewCell {
         self.textView.frame = CGRectMake(origin.x, origin.y, size.width, size.height)
         
         //view
-        let margin = Margin.Left + self.icon.frame.size.width + Margin.Right
+        let margin = self.icon.frame.size.width + Margin.Width
         switch self.qualia.vector {
         case .Me:
-            let width = size.width + Margin.Left + Margin.Right // equal view size width
+            let width = size.width + Margin.Width // equal view size width
             let x     = self.frame.size.width - margin - width  // view-icon-|
-            self.view.frame = CGRectMake(x, 0, width, size.height + Margin.Top + Margin.Bottom)
+            self.view.frame           = CGRectMake(x, 0, width, size.height + Margin.Height)
+            self.view.backgroundColor = UIColor.blackColor()
             
         case .Peer:
             // margin = |-icon-view
-            self.view.frame = CGRectMake(margin, 0, size.width + Margin.Left + Margin.Right, size.height + Margin.Top + Margin.Bottom)
+            self.view.frame           = CGRectMake(margin, 0, size.width + Margin.Width, size.height + Margin.Height)
+            self.view.backgroundColor = UIColor.whiteColor()
         }
         
     }
